3ae9745884478f00a973f4b28e60ba67be0ed2c7
Samuele Salvatico <samuele.salvatico@nearform.com>
2024-11-15 18:40:36 +0100

    chore: added under pressure

---
+++ src/plugins/external/under-pressure.ts
@@ -0,0 +1,23 @@
+import fastifyUnderPressure from "@fastify/under-pressure";
+import type { FastifyReply, FastifyRequest } from "fastify";
+import createHttpError from "http-errors";
+import v8 from "v8";
+
+export const autoConfig = {
+  maxEventLoopDelay: 1000,
+  maxHeapUsedBytes: v8.getHeapStatistics().heap_size_limit,
+  maxRssBytes: v8.getHeapStatistics().total_available_size,
+  maxEventLoopUtilization: 0.98,
+  pressureHandler: (
+    _req: FastifyRequest,
+    _rep: FastifyReply,
+    type: string,
+    value: number | string | undefined | null,
+  ) => {
+    throw createHttpError.ServiceUnavailable(
+      `System is under pressure. Pressure type: ${type}. Pressure value: ${value}`,
+    );
+  },
+};
+
+export default fastifyUnderPressure;
